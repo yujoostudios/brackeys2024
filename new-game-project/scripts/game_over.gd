d8eb7566269779932f9cdbe153e06d3a1b0f14b6
angelaaaaliu <liu.angela.1@gmail.com>
2024-09-14 12:47:43 -0700

    powerups
    added powerup sprite sheets

---
+++ new-game-project/scripts/game_over.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://screens/scenes/outside_map.tscn")
+
+func _on_quit_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://screens/scenes/start_menu.tscn")
