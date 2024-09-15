c60f73ce3d10b2893b2f0004ed2503d8907b3978
angelaaaaliu <liu.angela.1@gmail.com>
2024-09-14 18:11:41 -0700

    audio fix

--- new-game-project/scripts/outside_map.gd
+++ new-game-project/scripts/outside_map.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$SneakingAudio.play_sneaking_music()
+	SneakingAudio.play_sneaking_music()
 	pass # Replace with function body.
 
 
